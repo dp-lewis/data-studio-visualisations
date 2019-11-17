ca5bfc40814b2aa76c8de12ca2690e380d9f06d6
David Lewis <mail@david-lewis.com>
2019-11-17 21:00:01 +1100

    Deep merge the manifest

--- src/createmanifest.js
+++ src/createmanifest.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
+const _ = require('lodash');
 const defaultConfig = require('../manifest/default.json');
 const stagingConfig = require('../manifest/staging.json');
 const productionConfig = require('../manifest/production.json');
 const environment = process.env.NODE_ENV || 'production';
 let outputConfig = {};
+
 if (environment === 'production') {
-  outputConfig = Object.assign(defaultConfig, productionConfig);
+  outputConfig = _.merge(defaultConfig, productionConfig);
 } else {
-  outputConfig = Object.assign(defaultConfig, stagingConfig);  
+  outputConfig = _.merge(defaultConfig, stagingConfig);
 }
 
 fs.writeFileSync('./build/manifest.json', JSON.stringify(outputConfig, null, 2));
